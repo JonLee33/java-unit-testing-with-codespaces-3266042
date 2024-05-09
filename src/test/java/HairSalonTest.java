a0f0eca646347c7c8f096aa96ba3b42e7d7181e7
JonLee33 <lee.jonathan.p@gmail.com>
2024-05-09 18:18:32 +0000

    challenge

--- src/test/java/HairSalonTest.java
+++ src/test/java/HairSalonTest.java
@@ -1,3 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class HairSalonTest {
@@ -6,21 +12,44 @@ class HairSalonTest {
 
     @Test
     public void testGetOpeningDays() {
+        // compare arrays
+
+        String[] expected = new String[]{"Monday", "Tuesday"};
+
+        String[] actual = hairSalon.getOpeningDays();
+
+        assertArrayEquals(expected, actual);
 
     }
 
     @Test
     public void testSalonIsOpenOnMonday() {
+        // boolean
 
+        boolean actual = hairSalon.isOpen("Monday");
+
+        assertTrue(actual);
     }
 
     @Test
     public void testSalonIsClosedOnWednesday() {
+        // boolean
 
+        boolean actual = hairSalon.isOpen("Wednesday");
+
+        assertFalse(actual);
     }
 
     @Test
     public void testExceptionIsThrownIfWeekdayDoesNotEndInDay() {
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> hairSalon.isOpen("June"));
+
+        String expected = "Only weekdays are valid";
+
+        String actual = exception.getMessage();
+
+        assertEquals(expected, actual);
 
     }
 
